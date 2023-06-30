bb1b0fc9261f2a3d4ea0f613f4bf1162b0b49541
OksanaShypik <kipishoks@gmail.com>
2023-06-30 23:05:44 +0200

    chenge logic

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,23 @@
 import throttle from 'lodash.throttle';
-
 const form = document.querySelector('.feedback-form');
-let dataForm = {};
-
-
 const FORM = 'feedback-form-state';
-const memory = JSON.parse(localStorage.getItem(FORM));
+let dataForm = (JSON.parse(localStorage.getItem(FORM)) || {});
 
+dataLocal();
 
-
-dataLocal()
+function dataLocal(email, message) { 
+    if (dataForm.email || 0) {
+        form.email.value = dataForm.email;
+    };
+    if (dataForm.message || 0) {
+         form.message.value = dataForm.message;
+    };
+       }
+;
 
 form.addEventListener('input', throttle(saveData, 1000));
 
 function saveData(e) {
-    if (memory) {
-    if (memory.email) {
-        dataForm.email = memory.email;
-         };
-    if (memory.message) {
-         dataForm.message = memory.message;
-           }
-   };
     dataForm[e.target.name] = e.target.value;
     localStorage.setItem(FORM, JSON.stringify(dataForm))
 };
@@ -36,20 +32,66 @@ function sendForm(evt) {
     localStorage.removeItem(FORM);    
     } else {
         evt.preventDefault();
-        return (console.log('Check the data please'));
+        return (alert('Check the data please'));
         dataForm = {};
     }
 };
 
-function dataLocal() { 
-    const saveForm = JSON.parse(localStorage.getItem(FORM));
-    if (saveForm) {
-            console.log('email:', saveForm.email);
-        form.email.value = saveForm.email;
-         console.log('message:', saveForm.message);
-        form.message.value = saveForm.message;
-    }
+
+
+// const form = document.querySelector('.feedback-form');
+// let dataForm = {};
+
+
+// const FORM = 'feedback-form-state';
+// const memory = JSON.parse(localStorage.getItem(FORM));
+
+// dataLocal()
+
+// form.addEventListener('input', throttle(saveData, 1000));
+
+// function saveData(e) {
+//     if (memory) {
+//     if (memory.email) {
+//         dataForm.email = memory.email;
+//          };
+//     if (memory.message) {
+//          dataForm.message = memory.message;
+//            }
+//    };
+//     dataForm[e.target.name] = e.target.value;
+//     localStorage.setItem(FORM, JSON.stringify(dataForm))
+// };
+
+// form.addEventListener('submit', sendForm);
+// function sendForm(evt) {
+//     if ((dataForm.email && dataForm.message)) {
+//     evt.preventDefault();
+//     console.log(dataForm);
+//     evt.currentTarget.reset();
+//     dataForm = {};
+//     localStorage.removeItem(FORM);    
+//     } else {
+//         evt.preventDefault();
+//         return (alert('Check the data please'));
+//         dataForm = {};
+//     }
+// };
+
+// function dataLocal(email, message) { 
+//         console.log(memory);
+//     // const saveForm = JSON.parse(localStorage.getItem(FORM));
+//     // console.log(saveForm);
+//     if (memory) {
+//             // console.log('email:', memory.email);
+//         form.email.value = memory.email;
+//         //  console.log('message:', saveForm.message);
+//         form.message.value = memory.message;
+//         dataForm.email = memory.email;
+//         dataForm.message = memory.message;
+//         console.log(dataForm);
+//     }
    
 
-};
+// };
 
